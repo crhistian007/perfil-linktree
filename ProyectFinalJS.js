cdcf61144d69cccaa9624e91dabf385c585ffe3a
crhistian <crhistian.castaneda.b@uni.pe>
2025-04-02 20:51:01 -0500

    Proyecto Final

---
+++ ProyectFinalJS.js
@@ -0,0 +1,15 @@
+document.getElementById("nombre").textContent = "Crhistian Castañeda Balcazar";
+document.getElementById("descripcion").textContent = "Estudiante para desarrollo web.";
+
+
+function Twiter(){
+    window.open("https://x.com/IRONMANtri", "_blank");
+}
+
+function Merch(){
+    window.open("https://www.bonfire.com/online-stores/", "_blank");
+}
+
+function Contact(){
+    window.open("https://www.facebook.com/crhistian.castaneda.1", "_blank");
+}
